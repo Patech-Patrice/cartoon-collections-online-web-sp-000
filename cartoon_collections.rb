71d672f20ecbc935366350a0c0ddeff2cd713fe6
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-16 00:14:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ end
 
 def summon_captain_planet(array)
   planeteer_calls = []
-  # Your code here
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+  end
+  planeteer_calls
 end
 
 def long_planeteer_calls(array)# code an argument here
